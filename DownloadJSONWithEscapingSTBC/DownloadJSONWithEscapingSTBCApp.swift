e119982c25e7e1c6076c02899e4c4a6d6360b579
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-03 12:08:08 +0300

    Initial Commit

---
+++ DownloadJSONWithEscapingSTBC/DownloadJSONWithEscapingSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadJSONWithEscapingSTBCApp.swift
+//  DownloadJSONWithEscapingSTBC
+//
+//  Created by Vahtee Boo on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DownloadJSONWithEscapingSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
